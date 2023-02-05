1db3aa5b1522cd97fe52557b1e55874f62f77175
lije <lije0009@gmail.com>
2023-02-05 23:07:23 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스 p.203

--- src/main/java/com/springboot/book/project1/web/IndexController.java
+++ src/main/java/com/springboot/book/project1/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.springboot.book.project1.web;
 
+import com.springboot.book.project1.config.auth.LoginUser;
+import com.springboot.book.project1.config.auth.dto.SessionUser;
 import com.springboot.book.project1.service.PostsService;
 import com.springboot.book.project1.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+       // SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
