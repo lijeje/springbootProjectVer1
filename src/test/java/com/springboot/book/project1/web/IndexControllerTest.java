2a432a37035e0b7a4304baf7cf147a329c5dd2f8
lije <lije0009@gmail.com>
2023-02-15 07:13:50 +0900

    5장 마무리

--- src/test/java/com/springboot/book/project1/web/IndexControllerTest.java
+++ src/test/java/com/springboot/book/project1/web/IndexControllerTest.java
@@ -20,7 +20,7 @@ public class IndexControllerTest {
     public void 메인페이지_로딩(){
         //when
         String body = this.restTemplate.getForObject("/",String.class);
-
+        int[] answer = {};
         //then
         //assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
     }
